6c74564167db986eb23283833a13c9b90f427faa
Arthur Vendramin <arthurvendramin@gmail.com>
2026-06-18 09:44:12 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Titulo() {
+  return (
+    <>
+    <br/><br/>
+      <h2>Arthur Vendramin</h2>
+      <p>
+        Meu nome é Arthur Vendramin, curso técnico em <b>desnvolvimento de sistemas</b>, moro em Guaramirim SC
+      </p>
+      <br/>
+    </>
+  );
+}
+
+function Lista() {
+  return (
+    <>
+    <h4>3 tecnologias que desejo aprender na área de programação</h4>
+    <ul>
+      <li>C++</li>
+      <li>Phyton</li>
+      <li>JavaScript</li>
+      <li>Desenvolvimento de engines</li>
+    </ul>
+    </>
+  );
+}
+
+function ListaOrdenada() {
+  return (
+    <>
+    <h4>3 objetivos ou metas na área de tecnologia.</h4>
+    <ol>
+      <li>Faculdade na área</li>
+      <li>Alcançar um emprego que me agrade e seja bem remunerado</li>
+      <li>Não enlouquecer</li>
+    </ol>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Titulo/>
+        <Lista/>
+        <ListaOrdenada/>
+    </>
   );
 }
+
 
 export default App;
